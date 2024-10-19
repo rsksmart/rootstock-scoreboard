862d4fc6d978cdbc09b44ff1146668e63b911476
rookiecol <juan.diegoam@hotmail.com>
2024-10-19 17:00:55 +0700

    fix: validation to prevent overwriting existing leaders

--- contracts/TeamsManager.sol
+++ contracts/TeamsManager.sol
@@ -67,6 +67,7 @@ contract TeamsManager is Administrable, ReentrancyGuard {
 
   function addTeam(string memory teamName, address memeTokenAddress, address teamLeaderAddress) public {
     require(bytes(_teams[teamName].teamName).length == 0, "Team already added");
+    require(bytes(_teamLeaders[teamLeaderAddress]).length == 0, "Leader already assigned to a team");
 
     IERC20 memeTokenContract = IERC20(memeTokenAddress);
     string memory memeTokenName = memeTokenContract.name();
