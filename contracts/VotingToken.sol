336fa48c903069ba12263c6843979e7a492ea234
Jeremiah Samuel <livinalt@gmail.com>
2025-02-11 18:30:44 +0100

    fix: remove URI

--- contracts/VotingToken.sol
+++ contracts/VotingToken.sol
@@ -4,14 +4,9 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract VotingToken is ERC20 {
-    string private _uri;
 
-    constructor(uint256 initialSupply, string memory uri) ERC20("VotingToken", "VOTE") {
+    constructor(uint256 initialSupply) ERC20("VotingToken", "VOTE") {
         _mint(msg.sender, initialSupply);
-        _uri = uri;
     }
 
-    function getUri() external view returns (string memory) {
-        return _uri;
-    }
 }
