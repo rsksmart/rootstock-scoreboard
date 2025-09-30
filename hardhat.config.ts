62b4b8e624ed3b13acc005ebae6eab7ce9814ebe
TheSoftNode <thesoftnode@gmail.com>
2025-09-30 15:12:07 +0100

    feat: enable Solidity optimizer
    - Fix 'stack too deep' compilation error

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,12 +5,18 @@ import '@nomicfoundation/hardhat-toolbox';
 import { HardhatUserConfig } from 'hardhat/types';
 
 const config: HardhatUserConfig = {
-    solidity: '0.8.20',
+    solidity: {
+        version: '0.8.20',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    },
     paths: {
         root: './',
-
     },
-
 };
 
 module.exports = config;
